7df6917f207a8a7d435b018b6aefbc05c88acb0c
Thomas Chapuis <chapuis.t@gmail.com>
2020-04-06 13:29:08 +0200

    Fixed timezone

--- src/index.js
+++ src/index.js
@@ -9,9 +9,12 @@ client.once('ready', () => {
     console.log('Ready!');
         
     cron.schedule('15 9,12,13,17 * * *', () => {
+        console.log('sending message');
         client.channels.fetch(process.env.CHANNEL_ID).then(
             channel => channel.send('@everyone n\'oubliez pas d\'émarger !')
         );
+    }, {
+        timezone: "Europe/Paris"
     });
 });
 
