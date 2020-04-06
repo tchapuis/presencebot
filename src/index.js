2beb5bc0a60eb478679a073b99edbffd649971e0
Thomas Chapuis <chapuis.t@gmail.com>
2020-04-06 12:26:58 +0200

    Added message sending to correct channel

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 const config = require('./config.json');
 
 const Discord = require('discord.js');
+const cron = require('node-cron');
+
 const client = new Discord.Client();
 
 client.once('ready', () => {
-	console.log('Ready!');
+    console.log('Ready!');
+        
+    cron.schedule('15 9,12,13,17 * * *', () => {
+        client.channels.fetch(config.channelId).then(
+            channel => channel.send('@everyone n\'oubliez pas d\'émarger !')
+        );
+    });
 });
 
 client.login(config.token);
