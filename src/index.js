e54bf32cf755c6998ec8afa2c0b90542b22682d1
Thomas Chapuis <chapuis.t@gmail.com>
2020-04-06 16:46:41 +0200

    Migrated to cron lib for better TZ handling

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
 require('dotenv').config();
 
 const Discord = require('discord.js');
-const cron = require('node-cron');
+const CronJob = require('cron').CronJob;
 
 const client = new Discord.Client();
 
 client.once('ready', () => {
     console.log('Ready!');
         
-    cron.schedule('15 9,12,13,17 * * *', () => {
+    const job = new CronJob('15 9,12,13,17 * * *', () => {
         console.log('sending message');
         client.channels.fetch(process.env.CHANNEL_ID).then(
             channel => channel.send('@everyone n\'oubliez pas d\'émarger !')
         );
-    }, {
-        timezone: "Europe/Paris"
-    });
+    }, null, true, 'Europe/Paris');
+    
+    job.start();
 });
 
 client.login(process.env.TOKEN);
